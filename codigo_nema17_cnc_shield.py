57c9e67c3ada64dc114f8867a658483a1328f3c7
Bruno P. Iglesias <37714912+bruiglesias@users.noreply.github.com>
2021-01-11 09:08:21 -0400

    Update codigo_nema17_cnc_shield.py

--- codigo_nema17_cnc_shield.py
+++ codigo_nema17_cnc_shield.py
@@ -7,6 +7,12 @@
  MOVIMENTOS NO EIXO X E Y NO INTERVALO DE 0 A 30 CM
  SENSORES DE PARADA NA POSICAO 0 E PERSISTENCIA DA POSICAO NA EEPROM
 
+ RECEBE NA PORTA SERIAL UMA MENSAGEM TIPO 'M-1-1-30', ONDE: 
+ M : ACAO DE MOVIMENTO
+ 1 : MOTOR A SER ACIONADO (1 : EIXO X , 2 : EIXO Y)
+ 1 : SENTIDO DO MOVIMENTO NO CARTESIANO ( 1 : SENTIDO POSITIVO, 0: SENTIDO NEGATIVO )
+ 30: QUANTIDADE DE MOVIMENTO EM CENTIMETROS
+
  **/
 
 AccelStepper stepper1(1, 2 ,5);
@@ -14,25 +20,25 @@ AccelStepper stepper2(1, 3 ,6);
 
 
 int distancia_um_centimetro = 51.282051282;
-int distancia_cinco_centimetros = 256.41025641;  
-int distancia_dez_centimetros = 512.820512821; 
+
 
 int e_AddressX  = 0;
 int e_AddressY  = 512;
-
-int positionX = 0;
-int positionY = 0;
 
 int x_sensor = 9;
 int y_sensor = 10;
 int z_sensor = 11;
 
+String action;
+String axis;
+String direct;
+String value;
 
-
+int LIMIT = 30;
 
 void setup()
 { 
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     pinMode(x_sensor, INPUT_PULLUP);
     pinMode(y_sensor, INPUT_PULLUP);
@@ -40,35 +46,162 @@ void setup()
 
     stepper1.setMaxSpeed(8000.0);
     stepper1.setAcceleration(8000.0);
-    //stepper1.moveTo(movement);
    
-    stepper2.setMaxSpeed(8000.0);
-    stepper2.setAcceleration(8000.0);
-    
-    positionX = EEPROM.read(e_AddressX);
-    positionY = EEPROM.read(e_AddressY);
-    //stepper2.moveTo(movement);
+    stepper2.setMaxSpeed(7000.0);
+    stepper2.setAcceleration(7000.0);
+
+    if(digitalRead(x_sensor) == 1 || EEPROM.read(e_AddressX) > 30)
+    {
+      EEPROM.write(e_AddressX, 0);
+    }
+
+    if(digitalRead(y_sensor) == 1 || EEPROM.read(e_AddressY) > 30)
+    {
+      EEPROM.write(e_AddressY, 0);
+    }
+
 }
 
-char inBuffer = "";
 
 void stopX()
 {
   stepper1.stop();
   stepper1.setCurrentPosition(0);
   stepper1.move(0);
-  positionX = 0;
-  EEPROM.write(e_AddressX, positionX);
+  EEPROM.write(e_AddressX, 0);
 }
-
 
 void stopY()
 {
   stepper2.stop();
   stepper2.setCurrentPosition(0);
   stepper2.move(0);
-  positionY = 0;
-  EEPROM.write(e_AddressY, positionY);
+  EEPROM.write(e_AddressY, 0);
+}
+
+
+
+void setRun(){
+  
+  if (axis == "1" && direct == "0")
+  {  
+      if(digitalRead(x_sensor) == 1)
+      {
+        stopX();
+        Serial.println(EEPROM.read(e_AddressX));
+         
+      }
+      else
+      {
+        int dist = value.toInt();
+        stepper1.move(-dist * distancia_um_centimetro);
+        EEPROM.write(e_AddressX, EEPROM.read(e_AddressX) - dist);
+        Serial.println(EEPROM.read(e_AddressX));
+      }
+      
+    }
+
+  if (axis == "1" && direct == "1")
+    {  
+      if(EEPROM.read(e_AddressX) < LIMIT)
+      {
+       int dist = value.toInt();
+       
+       // verifica se a quantidade de movimento é maior que o limite da mesa
+       // se for irá movimentar apenas a distancia que falta para atigir o limite  
+       if(dist + EEPROM.read(e_AddressX) > 30){
+        dist = 30 - EEPROM.read(e_AddressX);
+       }
+       
+       stepper1.move(dist * distancia_um_centimetro);
+       EEPROM.write(e_AddressX, EEPROM.read(e_AddressX) + dist);
+       Serial.println(EEPROM.read(e_AddressX));
+      }
+      else
+      {
+        Serial.println(EEPROM.read(e_AddressX));
+      }   
+    }
+
+
+
+
+    if (axis == "2" && direct == "0")
+  {  
+      if(digitalRead(y_sensor) == 1)
+      {
+        stopY();
+        Serial.println(EEPROM.read(e_AddressY));
+         
+      }
+      else
+      {
+        int dist = value.toInt();
+        stepper2.move(-dist * distancia_um_centimetro);
+        EEPROM.write(e_AddressY, EEPROM.read(e_AddressY) - dist);
+        Serial.println(EEPROM.read(e_AddressY));
+      }
+      
+    }
+
+  if (axis == "2" && direct == "1")
+    {  
+      if(EEPROM.read(e_AddressY) < LIMIT)
+      {
+       int dist = value.toInt();
+       // verifica se a quantidade de movimento é maior que o limite da mesa
+       // se for irá movimentar apenas a distancia que falta para atigir o limite  
+       if(dist + EEPROM.read(e_AddressY) > 30){
+          dist = 30 - EEPROM.read(e_AddressY);
+        }
+       stepper2.move(dist * distancia_um_centimetro);
+       EEPROM.write(e_AddressY, EEPROM.read(e_AddressY) + dist);
+       Serial.println(EEPROM.read(e_AddressY));
+      }
+      else
+      {
+        Serial.println(EEPROM.read(e_AddressY));
+      }   
+    }
+
+   
+}
+
+
+void leStringSerial()
+{
+  
+  char caracter;
+  int i = 0;
+  String val;
+  
+  while(Serial.available() > 0)
+  {
+    caracter = Serial.read();
+    if(caracter != '\n'){
+      if(i == 0)
+      {
+        action = caracter;
+      }
+      else if(i == 2)
+      {
+        axis = caracter;
+      }
+      else if(i == 4)
+      {
+        direct = caracter;
+      }
+      else if( i == 6 || i == 7)
+      {
+        val.concat(caracter);
+        value = val;
+      }
+    }
+    delay(10);
+    i = i + 1;
+  }
+
+  setRun();
 }
 
 
@@ -77,228 +210,9 @@ void loop()
 
     if(Serial.available()>0)
     {
-      inBuffer = Serial.read();
+      leStringSerial();
     }
-
-     ////////////////////////////////////////////////////////////////////
-    //                      MOVIMENTOS NO EIXO X                      //
-   ////////////////////////////////////////////////////////////////////
-   /**
-   *        VOCABULARY
-   *        a = axis:x , distance: -10
-   *        b = axis:x , distance: 10
-   *        c = axis:x , distance: -5
-   *        d = axis:x , distance: 5
-   *        e = axis:x , distance: -1
-   *        f = axis:x , distance: 1
-   *        
-   **/
-   
-    if (inBuffer == 'a')
-    {
-      
-      if(digitalRead(x_sensor) == 1)
-      {
-        stopX();
-      }
-      else
-      {
-        if(positionX >= 10)
-        {
-          stepper1.move(-distancia_dez_centimetros);
-          positionX = positionX - 10;
-          EEPROM.write(e_AddressX, positionX);
-        }
-      }
-    }
-
-    if (inBuffer == 'b')
-    {  
-       if(positionX < 30)
-       {
-           stepper1.move(distancia_dez_centimetros);
-           positionX = positionX + 10;
-           EEPROM.write(e_AddressX, positionX);
-       } 
-    }
-
-    if (inBuffer == 'c')
-    {
-      
-      if(digitalRead(x_sensor) == 1)
-      {
-        stopX();
-      }
-      else
-      {
-        if(positionX >= 5)
-        {
-          stepper1.move(-distancia_cinco_centimetros);
-          positionX = positionX - 5;
-          EEPROM.write(e_AddressX, positionX); 
-        } 
-      }
-    }
-
-    if (inBuffer == 'd')
-    {  
-       if(positionX < 30)
-       {
-           stepper1.move(distancia_cinco_centimetros);
-           positionX = positionX + 5;
-           EEPROM.write(e_AddressX, positionX);
-       } 
-    }
-
-    if (inBuffer == 'e')
-    {
-      
-      if(digitalRead(x_sensor) == 1)
-      {
-        stopX();
-      }
-      else
-      {
-        if(positionX >= 1)
-        {
-          stepper1.move(-distancia_um_centimetro);
-          positionX = positionX - 1;
-          EEPROM.write(e_AddressX, positionX);
-        }  
-      }
-    }
-
-    if (inBuffer == 'f')
-    {  
-       if(positionX < 30)
-       {
-           stepper1.move(distancia_um_centimetro);
-           positionX = positionX + 1;
-           EEPROM.write(e_AddressX, positionX);
-       } 
-    }
-
-  
-     ////////////////////////////////////////////////////////////////////
-    //                      MOVIMENTOS NO EIXO Y                      //
-    ///////////////////////////////////////////////////////////////////
-    /**
-     *        VOCABULARY
-     *        g = axis:y , distance: -10
-     *        h = axis:y , distance: 10
-     *        i = axis:y , distance: -5
-     *        j = axis:y , distance: 5
-     *        k = axis:y , distance: -1
-     *        l = axis:x , distance: 1
-     *        
-     **/
-    
-    if (inBuffer == 'g')
-    {
-      
-      if(digitalRead(y_sensor) == 1)
-      {
-        stopY();
-      }
-      else
-      {
-        if(positionY >= 10)
-        {
-          stepper2.move(-distancia_dez_centimetros);
-          positionY = positionY - 10;
-          EEPROM.write(e_AddressY, positionY);
-        }
-      }
-    }
-
-    if (inBuffer == 'h')
-    {  
-       if(positionY < 30)
-       {
-           stepper2.move(distancia_dez_centimetros);
-           positionY = positionY + 10;
-           EEPROM.write(e_AddressY, positionY);
-       } 
-    }
-
-    if (inBuffer == 'i')
-    {
-      
-      if(digitalRead(y_sensor) == 1)
-      {
-        stopY();
-      }
-      else
-      {
-        if(positionY >= 5)
-        {
-          stepper2.move(-distancia_cinco_centimetros);
-          positionY = positionY - 5;
-          EEPROM.write(e_AddressY, positionY); 
-        } 
-      }
-    }
-
-    if (inBuffer == 'j')
-    {  
-       if(positionY < 30)
-       {
-           stepper2.move(distancia_cinco_centimetros);
-           positionY = positionY + 5;
-           EEPROM.write(e_AddressY, positionY);
-       } 
-    }
-
-    if (inBuffer == 'k')
-    {
-      
-      if(digitalRead(y_sensor) == 1)
-      {
-        stopY();
-      }
-      else
-      {
-        if(positionY >= 1)
-        {
-          stepper2.move(-distancia_um_centimetro);
-          positionY = positionY - 1;
-          EEPROM.write(e_AddressY, positionY);
-        }  
-      }
-    }
-
-    if (inBuffer == 'l')
-    {  
-       if(positionY < 30)
-       {
-           stepper2.move(distancia_um_centimetro);
-           positionY = positionY + 1;
-           EEPROM.write(e_AddressY, positionY);
-       } 
-    }
-
-    
-
-    // CONTROL RUN
-
-     if(inBuffer == 'a' && positionX == 0 )
-     {
-          
-     }
-     else
-     {
-       stepper1.run();
-     }
-
-     if(inBuffer == 's' && positionY == 0 )
-     {
-          
-     }
-     else
-     {
-       stepper2.run();
-     }
-          
-    inBuffer = ""; 
-    
+     
+      stepper1.run();
+      stepper2.run();     
 }
